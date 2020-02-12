1ab42eb5c8c9d0482315818ff0a261819217154a
Dane <wheaton.dane@gmail.com>
2020-02-11 19:48:54 -0800

    add proj
    added assets and project settings

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 10;
+
+    Rigidbody2D myRigidbody;
+    Vector2 input;
+
+    private void Start()
+    {
+        myRigidbody = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        myRigidbody.MovePosition((Vector2)myRigidbody.transform.position + input * speed);
+    }
+}
