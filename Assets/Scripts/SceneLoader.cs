fffa40a0c1b51d883ec3ee0f38f6dbbd52888a55
Dane <wheaton.dane@gmail.com>
2020-02-12 18:36:41 -0800

    basic navigation

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static Scenes lastScene;
+
+    public enum Scenes { EstuaryOverworld, Stump, Dock, Log }
+
+    [SerializeField] Scenes sceneToLoad;
+
+    BoxCollider2D triggerZone;
+
+    string overworldName = "EstuaryOverworld", stumpName = "Stump", dockName = "Dock", logName = "Log";
+
+    private void OnValidate()
+    {
+        if (!triggerZone)
+        {
+            BoxCollider2D myCollider = GetComponent<BoxCollider2D>();
+
+            if (!myCollider)
+            {
+                gameObject.AddComponent<BoxCollider2D>();
+            }
+
+            triggerZone = myCollider;
+            triggerZone.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        string nameOfSceneToLoad = SceneManager.GetActiveScene().name;
+
+        switch (sceneToLoad)
+        {
+            case Scenes.EstuaryOverworld:
+                nameOfSceneToLoad = overworldName;
+                break;
+            case Scenes.Stump:
+                nameOfSceneToLoad = stumpName;
+                break;
+            case Scenes.Dock:
+                nameOfSceneToLoad = dockName;
+                break;
+            case Scenes.Log:
+                nameOfSceneToLoad = logName;
+                break;
+        }
+
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == stumpName)
+        {
+            lastScene = Scenes.Stump;
+        }
+        else if (sceneName == dockName)
+        {
+            lastScene = Scenes.Dock;
+        }
+        else if (sceneName == logName)
+        {
+            lastScene = Scenes.Log;
+        }
+
+        SceneManager.LoadScene(nameOfSceneToLoad);
+    }
+}
